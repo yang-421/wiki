d6a0e50be320944e82b36eabee7457e8ff97eace
yang.421 <yang.421@tamu.edu>
2021-04-15 21:04:49 -0700

    customized configuration test added

--- src/main/java/com/fate/wiki/controller/TestController.java
+++ src/main/java/com/fate/wiki/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.fate.wiki.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -9,7 +9,8 @@ import java.util.Map;
 //@ResponseBody return a string or json
 //@Controller//return a page
 public class TestController {
-
+    @Value("${test.hello}")
+    private String testHello;
     /**
      * GET POST PUT DELETE
      *
@@ -24,6 +25,12 @@ public class TestController {
     //RequestMapping(value="/user/1", method = RequestMethod.GET);
     //RequestMapping(value="/user/1", method = RequestMethod.GET);
     //it supports all the requesting ways
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!" + testHello;
+    }
+
+
     @RequestMapping("/hello/post")
     public String helloPost(String name) {
         return "Hello dude, " + name;
