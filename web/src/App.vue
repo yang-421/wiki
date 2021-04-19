363b0e41927c03eb4d303044313ea6c8d53a7a16
yang.421 <yang.421@tamu.edu>
2021-04-18 21:12:47 -0700

    create the-footer component

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,22 +2,23 @@
   <a-layout id="components-layout-demo-top-side-2">
         <the-header></the-header>
 
-        <router-view/>
 
-      <a-layout-footer style="text-align: center">
-            Ant Design ©2018 Created by Ant UED
-          </a-layout-footer>
+        <router-view/>
+        <the-footer></the-footer>
+
     </a-layout>
 </template>
 
 <script lang="ts">
      import { defineComponent } from 'vue';
      import TheHeader from '@/components/the-header.vue'
+     import TheFooter from '@/components/the-footer.vue'
 
      export default defineComponent({
        name: 'app',
        components:{
             TheHeader,
+            TheFooter,
        },
      });
      </script>
